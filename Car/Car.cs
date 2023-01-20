37fac06bbaac802775366aabdedc888fe25c9a49
Marcus Medina <medina.jobb@outlook.com>
2023-01-20 10:13:57 +0100

    Add project files.

---
+++ Car/Car.cs
@@ -0,0 +1,34 @@
+﻿namespace Cars;
+public class Car
+{
+	public string Model { get; set; } = " Skrothög";
+	public int Speed { get; set; }
+
+	public int SpeedRatio { get; set; } = 7;
+
+	public void Accelerate()
+	{
+		Speed += SpeedRatio;
+	}
+
+	public void Brake()
+	{
+		Speed -= SpeedRatio;
+	}
+
+	public void IncreaseSpeed(int amount)
+	{
+		for (int i = Speed; i < amount; i++)
+		{
+			Speed++;
+		}
+	}
+
+	public void DecreaseSpeed(int amount)
+	{
+		for (int i = Speed; i > amount; i--)
+		{
+			Speed--;
+		}
+	}
+}
---
+++ CarsTests/CarTests.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cars;
+
+namespace Cars.Tests;
+
+[TestClass()]
+public class CarTests
+{
+	Car testCar;
+	[TestInitialize] // Det ska köras innan varje test
+	public void Setup()
+	{
+		testCar = new()
+		{
+			Model = "Volvo",
+			Speed = 100,
+			SpeedRatio=10
+		};
+	}
+
+
+	[TestMethod()]
+	public void AccelerateTest()
+	{
+		// Arrange
+		int expected = 110;
+		// Act
+		testCar.Accelerate();
+		var actual = testCar.Speed;
+		// Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[TestMethod()]
+	public void BrakeTest()
+	{
+		// Arrange
+		int expected = 90;
+		// Act
+		testCar.Brake();
+		var actual = testCar.Speed;
+		// Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[TestMethod()]
+	public void IncreaseSpeedTest()
+	{
+		// Arrange
+		int expected = 135;
+		// Act
+		testCar.IncreaseSpeed(135);
+		var actual = testCar.Speed;
+		// Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[TestMethod()]
+	public void IncreaseSpeedTest2()
+	{
+		// Arrange
+		int expected = 100;
+		int data = 90;
+		// Act
+		testCar.IncreaseSpeed(data);
+		var actual = testCar.Speed;
+		// Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[TestMethod()]
+	public void DecreaseSpeedTest()
+	{
+		// Arrange
+		int expected = 30;
+		// Act
+		testCar.DecreaseSpeed(expected);
+		var actual = testCar.Speed;
+		// Assert
+		Assert.AreEqual(expected, actual);
+	}
+
+	[TestMethod()]
+	public void DecreaseSpeedTest2()
+	{
+		// Arrange
+		int expected = 100;
+		int data = 120;
+		// Act
+		testCar.DecreaseSpeed(data);
+		var actual = testCar.Speed;
+		// Assert
+		Assert.AreEqual(expected, actual);
+	}
+}
